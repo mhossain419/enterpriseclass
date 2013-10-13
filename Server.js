297da05469609c7cf849923c1ca319f776c02dc8
mhossain419 <mhossain419@users.noreply.github.com>
2013-10-13 18:05:09 -0400

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,11 +1,26 @@
 var http = require('http');
+var url = require('url');
 var calculator = require('./m_calculator');
 
+function handleUnknown(res) {
+    res.writeHead(200, {
+        "Content-Type" : "text/plain"
+    });
+    res.write("Not Supported Yet");
+    res.end();
+}
+
 function handleRequest(req, res) {
+
+    var data = url.parse(req.url, true).query;
+    if (data.max === undefined || data.iteration === undefined) {
+        handleUnknown(res);
+    }
+    
     var result = {};
-    result['Plouf'] = calculator.plouffBig(180000000);
-    result['Bellard'] = calculator.bellardBig(180000000);
-    result['PI'] = calculator.calculatePI(1000);
+    result['Plouf'] = calculator.plouffBig(data.iteration);
+    result['Bellard'] = calculator.bellardBig(data.iteration);
+    result['PI'] = calculator.calculatePI(data.max);
     res.writeHead(200, {
         "Content-Type" : "application/json"
     });
@@ -13,4 +28,4 @@ function handleRequest(req, res) {
     res.end();
 }
 
-http.createServer(handleRequest).listen(1337, '127.0.0.1');
+http.createServer(handleRequest).listen(1337);
